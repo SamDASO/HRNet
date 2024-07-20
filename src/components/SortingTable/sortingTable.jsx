3b1932bd63d60f558abc15b45519338c5a7e25e7
Samanta DA SILVA <developpement.sam@gmail.com>
2024-07-20 18:18:45 +0200

    table working fine and integrated to the create page

---
+++ src/components/SortingTable/sortingTable.jsx
@@ -0,0 +1,275 @@
+import { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { visuallyHidden } from '@mui/utils';
+
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: 'firstName',
+    numeric: false,
+    disablePadding: false,
+    label: 'First Name',
+  },
+  {
+    id: 'lastName',
+    numeric: false,
+    disablePadding: false,
+    label: 'Last Name',
+  },
+  {
+    id: 'startDate',
+    numeric: false,
+    disablePadding: false,
+    label: 'Start Date',
+  },
+  {
+    id: 'department',
+    numeric: false,
+    disablePadding: false,
+    label: 'Departement',
+  },
+  {
+    id: 'dateOfBirth',
+    numeric: false,
+    disablePadding: false,
+    label: 'Date Of Birth',
+  },
+  {
+    id: 'street',
+    numeric: false,
+    disablePadding: false,
+    label: 'Street',
+  },
+  {
+    id: 'city',
+    numeric: false,
+    disablePadding: false,
+    label: 'City',
+  },
+  {
+    id: 'state',
+    numeric: false,
+    disablePadding: false,
+    label: 'State',
+  },
+  {
+    id: 'zipCode',
+    numeric: false,
+    disablePadding: false,
+    label: 'Zip Code',
+  },
+
+];
+
+
+function EnhancedTableHead(props) {
+  const { order, orderBy, onRequestSort } =
+    props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+
+function SortingTable({employeesData}) {
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('firstName');
+  const [page, setPage] = useState(0);
+  const [dense, setDense] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - employeesData.length) : 0;
+
+  const visibleRows = useMemo(
+    () =>
+      stableSort(employeesData, getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage,
+      ),
+    [order, orderBy, page, rowsPerPage, employeesData],
+  );
+
+  return (
+    <Box sx={{ width: '80%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={employeesData.length}
+            />
+            <TableBody>
+              {visibleRows.map((row) => {
+
+                return (
+                    <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={row.id}
+                    sx={{ cursor: 'pointer' }}
+                  >
+                    <TableCell>{row.firstName}</TableCell>
+                    <TableCell >{row.lastName}</TableCell>
+                    <TableCell >{row.startDate}</TableCell>
+                    <TableCell >{row.department}</TableCell>
+                    <TableCell >{row.dateOfBirth}</TableCell>
+                    <TableCell >{row.street}</TableCell>
+                    <TableCell >{row.city}</TableCell>
+                    <TableCell >{row.state}</TableCell>
+                    <TableCell >{row.zipCode}</TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={9} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={employeesData.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
+  );
+}
+
+SortingTable.propTypes = {
+employeesData: PropTypes.arrayOf(
+    PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        firstName: PropTypes.string.isRequired, 
+        lastName: PropTypes.string.isRequired, 
+        startDate: PropTypes.any.isRequired, 
+        department: PropTypes.string.isRequired, 
+        dateOfBirth: PropTypes.any.isRequired, 
+        street: PropTypes.string.isRequired, 
+        city: PropTypes.string.isRequired, 
+        state: PropTypes.string.isRequired, 
+        zipCode: PropTypes.any.isRequired
+    })
+).isRequired,
+}
+
+export default SortingTable;
--- src/pages/Create/create.jsx
+++ src/pages/Create/create.jsx
@@ -1,11 +1,41 @@
 import Header from "../../components/Header/header";
+import SortingTable from "../../components/SortingTable/sortingTable";
 import styles from "./create.module.scss"
 
+//data test - needs to be replace with the real data
+const data = [
+  {
+    id: 1,
+    firstName: 'firstName1',
+    lastName: 'lastName1',
+    startDate: 'startDate1',
+    department: 'department1',
+    dateOfBirth: 'dateOfBirth1',
+    street: 'street1' ,
+    city: 'city1',
+    state: 'state1',
+    zipCode: 'zipCode1',
+  },
+  {
+    id: 2,
+    firstName: 'firstName2',
+    lastName: 'lastName2',
+    startDate: 'startDate2',
+    department: 'department2',
+    dateOfBirth: 'dateOfBirth2',
+    street: 'street2' ,
+    city: 'city2',
+    state: 'state2',
+    zipCode: 'zipCode2',
+  },
+
+];
 function Create() {
 
   return (
       <div className={styles.container}>
         <Header title="Current Employees"/>
+        <SortingTable employeesData={data}/>
       </div>
   );
 }
